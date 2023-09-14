dafaa1c54616984e6f49c09ef05bfaa5261e8d12
ColinLi33 <colin8tor1206@gmail.com>
2023-09-13 23:23:52 -0700

    Changed everything to js, set up logging locations

---
+++ db.js
@@ -0,0 +1,92 @@
+const mysql = require('mysql2');
+
+class Database {
+	constructor(config) {
+		this.config = config;
+		this.connection = mysql.createConnection(config);
+	}
+    //connect to db
+	connect() {
+		return new Promise((resolve, reject) => {
+			this.connection.connect((err) => {
+				if (err) {
+					console.error('Error connecting to MySQL:', err);
+					reject(err);
+				} else {
+					console.log('Connected to MySQL');
+					resolve();
+				}
+			});
+		});
+	}
+    //set up DB if needed
+	async initialize() {
+		await this.connect();
+
+		// Create People table if it doesn't exist
+		await this.query(`
+			CREATE TABLE IF NOT EXISTS People (
+				person_id INT AUTO_INCREMENT PRIMARY KEY,
+				name VARCHAR(255) UNIQUE
+			)
+		`);
+
+		// Create LocationData table if it doesn't exist
+		await this.query(`
+			CREATE TABLE IF NOT EXISTS LocationData (
+				location_id INT AUTO_INCREMENT PRIMARY KEY,
+				person_name VARCHAR(255),
+				location POINT SRID 4326,
+				timestamp INT
+			)
+		`);
+
+		// Perform other initialization tasks here
+	}
+    //insert location data into DB
+    async insertLocationData(personName, latitude, longitude, timestamp) {
+        // Create a valid Point geometry with SRID 4326
+        const point = `POINT(${latitude} ${longitude})`;
+      
+        // Insert the data into the LocationData table, skipping the auto-incremented location_id
+        await this.query('INSERT INTO LocationData (person_name, location, timestamp) VALUES (?, ST_GeomFromText(?, 4326), ?)', [personName, point, timestamp]);
+      }
+
+	async getLocationData() {
+		// Retrieve Point data as text with SRID
+		const results = await this.query('SELECT *, AsText(location) AS location_text FROM LocationData');
+		return results.map((row) => {
+			const locationText = row.location_text;
+			// Parse locationText to extract latitude and longitude if needed
+			return {
+				...row,
+				latitude: parsedLatitude,
+				longitude: parsedLongitude,
+			};
+		});
+	}
+
+	query(sql, values = []) {
+		return new Promise((resolve, reject) => {
+			this.connection.query(sql, values, (err, results) => {
+				if (err) {
+					console.error('Error executing query:', err);
+					reject(err);
+				} else {
+					resolve(results);
+				}
+			});
+		});
+	}
+
+	close() {
+		this.connection.end((err) => {
+			if (err) {
+				console.error('Error closing the database:', err);
+			} else {
+				console.log('Database connection closed');
+			}
+		});
+	}
+}
+module.exports = Database;
---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+require('dotenv').config();
+const Life360 = require('./life360');
+const Database = require('./db')
+
+const port = 3333;
+const app = express();
+const dbConfig = {
+    host: 'localhost',
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME,
+};
+const db = new Database(dbConfig);
+
+app.get('/', (req, res) => {
+    res.send('Server is running.');
+});
+
+//Function to initialize and connect to Life360 and get a list of memebrs in the circle
+async function getMembers(){
+    try{
+        const life360Client = new Life360(process.env.LIFETOKEN, process.env.LIFEUSERNAME, process.env.LIFEPASSWORD);
+        if(await life360Client.authenticate()){
+            circles = await life360Client.getCircles();
+            circleId = circles[0]['id']; //get first circleId since i only have 1
+            circle = await life360Client.getCircle(circleId);
+            members = circle['members'];
+            return members;
+        }
+    
+    } catch(error){
+        console.error("ERROR SIGNING IN:", error);
+    }
+}
+
+//Function to get location of all members of circle and push it to MySQL database
+async function logData(members){
+    for(let i = 0; i < members.length; i++){
+        const name =  members[i]['firstName'] + members[i]['lastName'];
+        if(members[i]['location'] != null){
+            const lat = members[i]['location']['latitude'];
+            const long = members[i]['location']['longitude'];
+            const timestamp = members[i]['location']['timestamp'];
+            try {
+                // Insert location data into the database
+                await db.insertLocationData(name, lat, long, timestamp);
+                console.log(`Logged ${name}'s location`)
+                } catch(error){
+                    console.error('Error inserting location data:', error);
+            }
+        }
+    }
+}
+
+//start the MySQL database and start logging locations every 15 seconds
+async function startServer() {
+    try {
+        // Initialize the database
+        await db.initialize();
+        members = await getMembers();
+        // Start the Express server
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+
+        //runs logoData every 30 seconds
+        setInterval(function() {
+            logData(members);
+        }, 15 * 1000);
+    } catch(error){
+        console.error('Error initializing the database:', error);
+    }
+}
+
+startServer();
