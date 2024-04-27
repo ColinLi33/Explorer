038151938102126a9675e3d0df87f61e2cc46e56
ColinLi33 <Colin8tor1206@gmail.com>
2024-04-26 22:12:55 -0700

    Fixed bug with points super far away

--- public/js/maps.js
+++ public/js/maps.js
@@ -10,9 +10,30 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 var worldPolygon = turf.polygon([[[-90, -180], [-90, 180], [90, 180], [90, -180], [-90, -180]]]);
 
-var line = turf.lineString(points.map(point => [point.latitude, point.longitude]));
-var buffered = turf.buffer(line, 0.20); // Adjust the buffer size as needed
-worldPolygon = turf.difference(worldPolygon, buffered);
+const maxDistance = 10; //max distance in km between two points
+var segments = [];
+var segment = [points[0]];
+
+for (let i = 1; i < points.length; i++) {
+    const point1 = turf.point([segment[segment.length - 1].longitude, segment[segment.length - 1].latitude]);
+    const point2 = turf.point([points[i].longitude, points[i].latitude]);
+    const distance = turf.distance(point1, point2);
+
+    if (distance > maxDistance) {
+        segments.push(segment);
+        segment = [points[i]];
+    } else {
+        segment.push(points[i]);
+    }
+}
+segments.push(segment);
+
+segments.forEach(segment => {
+    var line = turf.lineString(segment.map(point => [point.latitude, point.longitude]));
+    var buffered = turf.buffer(line, 0.20); 
+    worldPolygon = turf.difference(worldPolygon, buffered);
+});
+
 var resultPolygon = L.polygon(worldPolygon.geometry.coordinates, {
     color: 'black',
     fillColor: 'black',
