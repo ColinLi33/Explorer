f94c431083c4f284099344a68cb7530b3237df19
ColinLi33 <Colin8tor1206@gmail.com>
2024-04-26 22:36:29 -0700

    work on fixing chaotic lines

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,7 +1,6 @@
 const mapElement = document.getElementById('map');
 const pointsData = mapElement.getAttribute('data-points');
 const points = JSON.parse(pointsData);
-
 var map = L.map('map').setView([points[0].latitude, points[0].longitude], 14); // Default center and zoom level
 map.getRenderer(map).options.padding = 100;
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,7 +8,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     dragging: true
 }).addTo(map);
 var worldPolygon = turf.polygon([[[-90, -180], [-90, 180], [90, 180], [90, -180], [-90, -180]]]);
-
 const maxDistance = 10; //max distance in km between two points
 var segments = [];
 var segment = [points[0]];
@@ -30,12 +28,12 @@ segments.push(segment);
 
 segments.forEach(segment => {
     if (segment.length > 1) {
-        var line = turf.lineString(segment.map(point => [point.latitude, point.longitude]));
+        var simplifiedSegment = simplify(segment.map(point => ({x: point.latitude, y: point.longitude})), 0.001, false);
+        var line = turf.lineString(simplifiedSegment.map(point => [point.x, point.y]));
         var buffered = turf.buffer(line, 0.20); // Adjust the buffer size as needed
         worldPolygon = turf.difference(worldPolygon, buffered);
     }
 });
-
 var resultPolygon = L.polygon(worldPolygon.geometry.coordinates, {
     color: 'black',
     fillColor: 'black',
