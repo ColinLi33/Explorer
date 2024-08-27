a4fe05c69c54708fb90d0f8443cb6b5800ffb91d
ColinLi33 <Colin8tor1206@gmail.com>
2024-08-27 00:03:23 -0700

    almost there I can feel it

--- index.js
+++ index.js
@@ -260,24 +260,22 @@ app.post('/updatePrivacy', authenticate, async (req, res) => { //update privacy 
 });
 
 app.post('/github-webhook', (req, res) => { //github webhook
-    console.log('Received a push event from Github');
     console.log(req.headers['x-hub-signature']);
-    req.on('data', function(chunk) {
-        console.log("TESTING")
-        let sig = "sha1=" + crypto.createHmac('sha1', process.env.GITHUB_SECRET).update(chunk.toString()).digest('hex')
-        if (req.headers['x-hub-signature'] == sig) {
-            logs.info('Received a push event from Github');
-            exec('sh ./deploy.sh', (error, stdout, stderr) => {
-                if (error) {
-                    logs.error(`Error executing deploy script: ${error}`);
-                    return;
-                }
-                logs.info(`Deploy script output: ${stdout}`);
-            });
-        } else {
-            logs.error('Invalid signature');
-        }
-    });
+    let sig = "sha1=" + crypto.createHmac('sha1', process.env.GITHUB_SECRET).digest('hex')
+    console.log(sig)
+    if (req.headers['x-hub-signature'] == sig) {
+        logs.info('Received a push event from Github');
+        exec('sh ./deploy.sh', (error, stdout, stderr) => {
+            if (error) {
+                logs.error(`Error executing deploy script: ${error}`);
+                return;
+            }
+            logs.info(`Deploy script output: ${stdout}`);
+        });
+    } else {
+        logs.error('Invalid signature');
+    }
+});
     
     res.status(202).send('Accepted');
 });
