2a8cc6edb9f9facdb8317679199e16afffd8276d
ColinLi33 <Colin8tor1206@gmail.com>
2024-08-27 00:02:16 -0700

    test

--- index.js
+++ index.js
@@ -261,6 +261,7 @@ app.post('/updatePrivacy', authenticate, async (req, res) => { //update privacy 
 
 app.post('/github-webhook', (req, res) => { //github webhook
     console.log('Received a push event from Github');
+    console.log(req.headers['x-hub-signature']);
     req.on('data', function(chunk) {
         console.log("TESTING")
         let sig = "sha1=" + crypto.createHmac('sha1', process.env.GITHUB_SECRET).update(chunk.toString()).digest('hex')
