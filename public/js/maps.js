c753ff5290e25110bbe3c6ebff0de5270de108d5
ColinLi33 <Colin8tor1206@gmail.com>
2025-11-20 22:56:51 -0500

    globe works

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,43 +1,31 @@
 const mapElement = document.getElementById('map');
 const pointsData = mapElement.getAttribute('data-points');
 const points = JSON.parse(pointsData);
-var map = L.map('map').setView([points[points.length-1].latitude, points[points.length-1].longitude], 14); // Default center and zoom level
-map.getRenderer(map).options.padding = 100;
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    noWrap: true,
-    dragging: true
-}).addTo(map);
-var worldPolygon = turf.polygon([[[-90, -180], [-90, 180], [90, 180], [90, -180], [-90, -180]]]);
-const maxDistance = 3.5; //max distance in km between two points
-var segments = [];
-var segment = [points[0]];
-for (let i = 1; i < points.length; i++) {
-    const point1 = turf.point([segment[segment.length - 1].longitude, segment[segment.length - 1].latitude]); //last point in segment
-    const point2 = turf.point([points[i].longitude, points[i].latitude]); //new point we are looking at
-    const distance = turf.distance(point1, point2);
+console.log(points)
 
-    if (distance > maxDistance) {
-        segments.push(segment);
-        segment = [points[i]];
-    } else {
-        segment.push(points[i]);
-    }
-}
-segments.push(segment);
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzZjViMTkyZC03MjNlLTQ5NWMtYWVhYi1lM2EyNWVhZTBiNWUiLCJpZCI6MzYyNDA5LCJpYXQiOjE3NjM2OTMzMTF9._xqwTlJIDkfBqCLu2cLPNdttN_Q5tSBh2TUdt6zGzaQ';
 
-segments.forEach(segment => {
-    if (segment.length > 1) {
-        var line = turf.lineString(segment.map(point => [point.latitude, point.longitude]));
-        var buffered = turf.buffer(line, 0.20); //how wide the line is 
-        worldPolygon = turf.difference(worldPolygon, buffered);
-    } else if(segment.length == 1) {  ///single point
-        var point = turf.point([segment[0].latitude, segment[0].longitude]);
-        var buffered = turf.buffer(point, 0.20); 
-        worldPolygon = turf.difference(worldPolygon, buffered);
-    }
+const viewer = new Cesium.Viewer("map", {
+    geocoder: false,
+    homeButton: false,
+    sceneModePicker: false,
+    baseLayerPicker: false,
+    navigationHelpButton: false,
+    animation: false,
+    timeline: false,
+    fullscreenButton: false,
+    vrButton: false,
+    infoBox: false,
+    selectionIndicator: false
 });
-var resultPolygon = L.polygon(worldPolygon.geometry.coordinates, {
-    color: 'black',
-    fillColor: 'black',
-    fillOpacity: 0.7
-}).addTo(map);
+
+try {
+    const imageryLayer = viewer.imageryLayers.addImageryProvider(
+        await Cesium.IonImageryProvider.fromAssetId(3830184),
+    );
+    await viewer.zoomTo(imageryLayer);
+} catch (error) {
+    console.log(error);
+}
+
+viewer.cesiumWidget.creditContainer.style.display = 'none';
